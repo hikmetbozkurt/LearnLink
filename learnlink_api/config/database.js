e9afe4f6324ab32a857e4802fef1fb1eeb32f863
Hikmet Bozkurt Aydoğan <hkmtbzkrt06@gmail.com>
2025-02-24 00:26:48 +0300

    Setup is updated.

--- learnlink_api/config/database.js
+++ learnlink_api/config/database.js
@@ -1,5 +1,5 @@
-import pg from 'pg'
-import config from './env.js'
+import pg from "pg";
+import config from "./env.js";
 
 const pool = new pg.Pool({
   user: config.DB_USER,
@@ -7,10 +7,13 @@ const pool = new pg.Pool({
   database: config.DB_NAME,
   password: config.DB_PASSWORD,
   port: config.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
   process.exit(-1);
 });
 
@@ -18,13 +21,13 @@ pool.on('error', (err) => {
 const testConnection = async () => {
   try {
     const client = await pool.connect();
-    console.log('Database connected successfully');
+    console.log("Database connected successfully");
     client.release();
   } catch (err) {
-    console.error('Database connection error:', err);
+    console.error("Database connection error:", err);
   }
 };
 
 testConnection();
 
-export default pool; 
+export default pool;
--- learnlink_api/config/env.js
+++ learnlink_api/config/env.js
@@ -6,10 +6,10 @@ const config = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   
   // Database
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_USER: process.env.DB_USER || 'keremtegiz',
+  DB_HOST: process.env.DB_HOST || 'database-1.cri0ugym88zn.eu-north-1.rds.amazonaws.com',
+  DB_USER: process.env.DB_USER || 'learlink',
   DB_PASSWORD: process.env.DB_PASSWORD || 'learnlink',
-  DB_NAME: process.env.DB_NAME || 'learnlink',
+  DB_NAME: process.env.DB_NAME || 'postgres',
   DB_PORT: process.env.DB_PORT || 5432,
   
   // JWT
