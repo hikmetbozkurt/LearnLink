575c5610232a77a4292208595a9cd9cb15313592
Kerem Tegiz <keremtegiz@gmail.com>
2025-04-09 19:19:49 +0300

    app.js updated.

--- learnlink_api/app.js
+++ learnlink_api/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import chatRoutes from './routes/chatRoutes.js';
@@ -12,11 +14,10 @@ import directMessageRoutes from './routes/directMessageRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
 import eventRoutes from './routes/eventRoutes.js';
-import fs from 'fs';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
